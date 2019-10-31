73f81556c87c734ebea33e78e7a5fc4f78b1e753
saravanks <ks.saravn@gmail.com>
2019-10-31 16:48:15 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://boring-noyce-c8be65.netlify.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: 'https://boring-noyce-c8be65.netlify.com', // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
